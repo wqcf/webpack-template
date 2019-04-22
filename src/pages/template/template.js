87cebf70e08e29564272bb804519708a2ae55f5a
wq <18266854539@163.com>
2019-04-22 14:23:14 +0800

    src

--- src/pages/template/template.js
+++ src/pages/template/template.js
@@ -4,12 +4,10 @@ require('cesium/Widgets/widgets.css');
 
 import _ from 'lodash';
 import * as $ from 'jquery';
+//Cesium
 // import MapControl from '../../js/lib/map';
 // import Cesium from 'cesium/Cesium';
-
 // var mapCon = new MapControl('cesiumContainer', false, false);
-
 // mapCon.GoMain();
 
-//系统时间
 
